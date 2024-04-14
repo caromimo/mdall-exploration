6d3cd09480d23f19c5e1397e52423a49a35e7e73
Caroline Mimeault <carolinemimeault@gmail.com>
2024-04-14 13:41:20 -0400

    changing migrations

---
+++ migrations/000001_create_tables.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "companies";
+DROP TABLE IF EXISTS "licences";
+DROP TABLE IF EXISTS "devices";
+DROP TABLE IF EXISTS "licence_types";
+DROP TABLE IF EXISTS "licences_devices";
---
+++ migrations/000001_create_tables.up.sql
@@ -0,0 +1,51 @@
+CREATE TABLE "companies" (
+  "id" integer PRIMARY KEY UNIQUE,
+  "name" text,
+  "address" text,
+  "postal_code" text,
+  "city" text,
+  "country" text,
+  "region" text,
+  "status" text
+);
+
+
+CREATE TABLE "devices" (
+  "id" integer PRIMARY KEY UNIQUE,
+  "name" text,
+  "licence_number" integer
+);
+
+CREATE TYPE typecode AS ENUM ('D', 'S', 'K', 'F', 'G', 'Y', '');
+CREATE TYPE typedescription AS ENUM (
+  'Single Device', 
+  'System', 
+  'Test Kit', 
+  'Device Family', 
+  'Device Group', 
+  'Device Group Family', 
+  'Unknown'
+  );
+
+CREATE TABLE "licence_types" (
+  "id" integer PRIMARY KEY UNIQUE,
+  "code" typecode,
+  "description" typedescription
+);
+
+CREATE TABLE "licences" (
+  "number" integer PRIMARY KEY UNIQUE,
+  "status" text,
+  "risk_class" integer,
+  "created_on" date,
+  "refreshed_on" date,
+  "expired_on" date,
+  "licence_type_id" integer REFERENCES licence_types(id),
+  "company_id" integer REFERENCES companies(id)
+);
+
+CREATE TABLE "licences_devices" (
+  "id" integer PRIMARY KEY UNIQUE,
+  "licence_number" integer REFERENCES licences(number),
+  "device_id" integer REFERENCES devices(id)
+);
