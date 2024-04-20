6925702529b7cc21b5720279b79254b121ee211d
Caroline Mimeault <carolinemimeault@gmail.com>
2024-04-20 16:42:13 -0400

    adding notes about things to fix

--- scripts/licences.py
+++ scripts/licences.py
@@ -18,12 +18,14 @@ with open("./data/licences.json", "rb") as file:
             VALUES (:number, :status, :risk_class, :created_on, :refreshed_on, :expired_on, :licence_type_id, :company_id)""",
             number=record["original_licence_no"],
             status=record["licence_status"],
-            risk_class=["appl_risk_class"],
+            risk_class=record["appl_risk_class"],
             created_on=record["first_licence_status_dt"],
             refreshed_on=record["last_refresh_dt"],
             expired_on=record["end_date"],
-            licence_type_id=["licence_type_id"],
-            company_id=["company_id"],
+            licence_type_id=record[
+                "licence_type_cd"
+            ],  # FIXME use the code to pull the id from the licence_type table
+            company_id=record["company_id"],  # FIXME most likely similar to above
         )
 
 con.close()
