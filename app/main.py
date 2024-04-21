7956fafaa1297dd97158bbe2dc7f93fa91a0f681
Caroline Mimeault <carolinemimeault@gmail.com>
2024-04-21 10:52:40 -0400

    basic testing of the API

--- app/main.py
+++ app/main.py
@@ -5,4 +5,5 @@ app = FastAPI()
 
 @app.get("/")
 async def read_main():
+    # query the database here
     return {"msg": "Hello World"}
--- app/test_main.py
+++ app/test_main.py
@@ -9,3 +9,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_popcorn():
+    response = client.get("/other")
+    assert response.status_code == 404
